6291c3c084f3be54485dddcc7f89a4bd261a2d40
“eyluldnzgit config --global user.email “eyluldnzcn@gmail.com <eyluldnzcn@gmail.com>
2022-01-07 23:40:26 +0300

    Makeup Footer and Navbar

--- src/components/base/Footer.jsx
+++ src/components/base/Footer.jsx
@@ -2,8 +2,33 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className=" bg-dark  mt-5 py-5">
+            <div className="container">
+                <div className="row  pb-4">
+                    <div className="col-6 pb-3">
+                        <h5 className="text-light">Team Member</h5>
+                        <ul className='list-unstyled mt-3'>
+                            <li className="text-light">
+                                <p>
+                                    Maide KIRAZ : maidekiraz@gmail.com
+                                </p>
+                            </li>
+                            <li className="text-light"><p>
+                                Naşide YILDIRIM : naside.yldrm21@gmail.com
+                            </p>
+                            </li>
+                            <li className="text-light"><p>
+
+                                Eylül Deniz Can : eyluldnzcn@gmail.com
+                            </p>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="col-6">
+                    <h5 className="text-light">Technologies</h5> </div>
+                </div>
+            </div>
+
+        </footer>
     )
 }
